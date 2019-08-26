59cf12ffc8d11a34c1f058c9e3f9318afc13342c
guochen <guochen@zhuoyuan-info.com>
2019-08-26 17:27:42 +0800

    commit

---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+	 <h1>Hello world!<nuxt-link to="/welcome">首页</nuxt-link></h1>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
---
+++ pages/welcome.vue
@@ -0,0 +1,9 @@
+<template>
+	<h2>欢迎光临<nuxt-link to="/">返回</nuxt-link></h2>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
